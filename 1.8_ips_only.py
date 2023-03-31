d921bd055a1fa1a682f99b450bd0611b180ef243
sudz4 <mwsuther@syr.edu>
2023-03-31 10:04:37 -0700

    we ride

--- 1.8_ips_only.py
+++ 1.8_ips_only.py
@@ -1,7 +1,24 @@
-# get IPs ONLY
-# this is good for quickly seeing IPs maybe with the hostname. those are probably real on the network or had been on the network
+"""get IPs only"""
 
+#libs
 import nmap
+import ipaddress
+import socket
+
+def normalize_ip_address(ip):
+    try:
+        return str(ipaddress.IPv4Address(ip))
+    except ipaddress.AddressValueError:
+        try:
+            return str(ipaddress.IPv6Address(ip))
+        except ipaddress.AddressValueError:
+            return None
+
+def resolve_hostname_to_ip(hostname):
+    try:
+        return socket.getaddrinfo(hostname, None, family=socket.AF_UNSPEC)
+    except socket.gaierror:
+        return None
 
 def get_ip_addresses(ip_range):
     nm = nmap.PortScanner()
@@ -9,7 +26,16 @@ def get_ip_addresses(ip_range):
     ip_addresses = []
 
     for host in nm.all_hosts():
-        ip_addresses.append(host)
+        normalized_ip = normalize_ip_address(host)
+        if normalized_ip:
+            ip_addresses.append(normalized_ip)
+        else:
+            resolved_ips = resolve_hostname_to_ip(host)
+            if resolved_ips:
+                for res in resolved_ips:
+                    family, _, _, _, addr = res
+                    if family == socket.AF_INET or family == socket.AF_INET6:
+                        ip_addresses.append(addr[0])
 
     return ip_addresses
 
